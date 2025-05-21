7c1568766fd9f883b8bb7ba51c243f6197196071
SanjayBalaji0 <sanjaybalajisaravanan2006@gmail.com>
2025-05-21 11:57:22 +0530

    redirect

---
+++ Assets/coding.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Coding : MonoBehaviour
+{
+    Rigidbody rb;
+    public GameObject WinText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Level2");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Cube")
+        {
+            Destroy(other.gameObject);
+            WinText.SetActive(true);
+        }
+    }
+}
